eb8b9671ca78ea7cefe01658bcc86d344d2f7292
Zhan Liu <zhan.liu128@gmail.com>
2026-09-18 16:22:01 +0100

    Contain contact CTA image

--- components/sections/FinalCTA.tsx
+++ components/sections/FinalCTA.tsx
@@ -13,9 +13,11 @@ export function FinalCTA() {
       <div className="final-cta__image">
         <Image
           src="/images/image-1.png"
-          fill
+          width={1536}
+          height={1024}
           sizes="(max-width: 820px) 100vw, 45vw"
           alt="Entrada da Clínica Beleza em Lisboa"
+          style={{ width: "100%", height: "100%", display: "block", objectFit: "cover" }}
         />
       </div>
       <Reveal className="final-cta__content">
